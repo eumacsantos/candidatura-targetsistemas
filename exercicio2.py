7fb4e307d2ef7f357bd306d383a6d93f4e443026
eumacsantos <eumacsantos@gmail.com>
2024-09-19 00:36:20 -0400

    deixando a primeira opção de resposta como resposta definitiva

--- exercicio2.py
+++ exercicio2.py
@@ -8,10 +8,10 @@ ocorrencia = 0
 ocorrencia = caracteres.count("a")
 
 # Segunda opção de resposta utilizando FOR para percorrer a string (lista de caracteres) e verificar a quantidade de vezes que a letra 'a' aparece
-ocorrencia = 0
-for letra in caracteres:
-    if letra == "a":
-        ocorrencia += 1
+#ocorrencia = 0
+#for letra in caracteres:
+    #if letra == "a":
+       #ocorrencia += 1
 
 # Imprimindo o número de vezes que a letra 'a' apareceu na string
 print(ocorrencia)
