410e451f2e0917a8f520c6c54ab6fe2e8e7c3221
eumacsantos <eumacsantos@gmail.com>
2024-09-19 00:29:47 -0400

    correção do exercício 5

--- exercicio5.py
+++ exercicio5.py
@@ -1,24 +1,53 @@
-# Variável que será associação entre interruptores e as suas respectivas salas
+# Inicialização das variáveis necessárias para resolução do problema
 interruptorSala = {}
-# Variável que receberá a observação do estado da lâmpada
+interruptores = [1,2,3]
+salas = ["A","B","C"]
+opcoesDeEstado = ["quente","ligada","desligada"]
 observacao = ""
 
-# Informando os procedimentos antes da observação do estado da lâmpada
-print("Primeira ida: Ligando o primeiro interruptor.")
-print("Segunda ida: Ligando o segundo interruptor e desligando o primeiro.")
+# Informando os procedimentos, antes da observação do estado da lâmpada na sala A
+print("Ligando o primeiro interruptor.")
+print("Esperando 5 minutos")
+print("Desligando o primeiro e ligando o segundo interruptor")
+print("Entrando na sala A.")
 
-# Inserindo a observação do estado da lâmpada
-while observacao != "ligada" and observacao != "desligada" and observacao != "quente":
-    observacao = input("Digite o estado da lâmpada (ligada, desligada, quente): ")
+# Inserindo a observação do estado da lâmpada da sala A
+while not(observacao in opcoesDeEstado):
+    observacao = input("Digite o estado da lâmpada (ligada, desligada ou quente): ")
     observacao = observacao.lower()
 
-# Fazendo a associação de interruptores e salas de acordo com a observação
+# Descobrindo o interruptor correto da sala A, de acordo com a observação feita
+for estado in opcoesDeEstado:
+    if observacao == estado:
+        interruptorSala[interruptores[opcoesDeEstado.index(observacao)]] = salas[0]
+        interruptores.pop(opcoesDeEstado.index(observacao))
+        salas.pop(0)
+        opcoesDeEstado.remove("quente")
+        break
+
+# Informando os procedimentos, antes da última ida permitida, na qual será na sala B
+print("Sainda da sala A e desligando o segundo interruptor.")
+print("Ligando o primeiro interruptor entre os interruptores restante.")
+print("Entrando na sala B.")
+
+# Inserindo a observação do estado da lâmpada da sala B
+observacao = ""
+while not(observacao in opcoesDeEstado):
+    observacao = input("Digite o estado da lâmpada (ligada ou desligada): ")
+    observacao = observacao.lower()
+
+# Descobrindo o interruptor correto da sala B, de acordo com a observação feita
 if observacao == "ligada":
-    interruptorSala = {1: "B", 2: "A", 3: "C"}  
-elif observacao == "desligada":
-    interruptorSala = {1: "A", 2: "B", 3: "C"}
+    interruptorSala[interruptores[0]] = salas[0]
+    interruptores.pop(0)
+    salas.pop(0)
 else:
-    interruptorSala = {1: "B", 2: "C", 3: "A"}
+    interruptorSala[interruptores[0]] = salas[1]
+    interruptores.pop(0)
+    salas.pop(1)
+
+# Associando a sala C com o último interruptor
+interruptorSala[interruptores[0]] = salas[0]
 
 # Imprimindo resultado
 print(interruptorSala)
